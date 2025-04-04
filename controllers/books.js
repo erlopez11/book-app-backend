67bb0eff7b7e5d0861fe91030f224b603c3a83c7
erlopez11 <lopezelainy@gmail.com>
2025-04-04 15:33:27 -0500

    wip:define create new bookStat route

--- controllers/books.js
+++ controllers/books.js
@@ -4,6 +4,20 @@ const User = require('../models/user');
 const Book = require('../models/book');
 const router = express.Router();
 
+//POST /books - CREATE Route "Protected"
+router.post('/', verifyToken, async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.user._id);
+        const bookStats = currentUser.bookStats;
+        bookStats.push(req.body);
+        await currentUser.save();
+        res.status(201).json(bookStats);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: error.message});
+    }
+});
+
 
 
 
