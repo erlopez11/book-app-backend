cffc9d4c292f3de4be2a7d6ada937560606b6834
erlopez11 <lopezelainy@gmail.com>
2025-04-04 16:02:43 -0500

    wip:define update bookStat route

--- controllers/books.js
+++ controllers/books.js
@@ -4,14 +4,28 @@ const User = require('../models/user');
 const Book = require('../models/book');
 const router = express.Router();
 
-//POST /books - CREATE Route "Protected"
+//POST /books - CREATE (Add new Book Stats) Route "Protected"
 router.post('/', verifyToken, async (req, res) => {
     try {
         const currentUser = await User.findById(req.user._id);
         const bookStats = currentUser.bookStats;
         bookStats.push(req.body);
         await currentUser.save();
-        res.status(201).json(bookStats);
+        res.status(201).json(bookStats); 
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: error.message});
+    }
+});
+
+//PUT /books/:bookId - UPDATE (Book Stats) Route "Protected"
+router.put('/:bookId', verifyToken, async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.user._id);
+        const bookStat = currentUser.bookStats.id(req.params.bookId);
+        bookStat.set(req.body);
+        await currentUser.save();
+        res.status(200).json({message: 'Update Successful'});
     } catch (error) {
         console.log(error);
         res.status(500).json({error: error.message});
