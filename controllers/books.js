87ed63824878cdc1844809b02e2bb500b3cd9cf7
erlopez11 <lopezelainy@gmail.com>
2025-04-04 16:31:28 -0500

    wip:define route to delete bookStat

--- controllers/books.js
+++ controllers/books.js
@@ -24,10 +24,28 @@ router.post('/', verifyToken, async (req, res) => {
 router.put('/:bookId', verifyToken, async (req, res) => {
     try {
         const currentUser = await User.findById(req.user._id);
+        //TODO: Can't use the bookId from params to access the bookStat blc that id will be set to book?
+        //need diff way to get that bookstat id or maybe the route id is set to the new bookStat?
+        //maybe it is ties to the book title?
+        //maybe insted of object ide use the isbn of the book since it is unique to the book?
         const bookStat = currentUser.bookStats.id(req.params.bookId);
         bookStat.set(req.body);
         await currentUser.save();
         res.status(200).json({message: 'Update Successful'});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: error.message});
+    }
+});
+
+//DELETE /books/:bookId - DELETE (Book Stats) Route "Protected"
+router.delete('/:bookId', verifyToken, async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.user._id);
+        const bookStats = currentUser.bookStats; 
+        bookStats.remove({_id: req.params.bookId});//TODO: same issue as above
+        await currentUser.save();
+        res.status(200).json({message: "Delete Successful!"});
     } catch (error) {
         console.log(error);
         res.status(500).json({error: error.message});
