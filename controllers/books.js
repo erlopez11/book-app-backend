7f03146bfe91103c4ad37766875b4f50673b3d3e
erlopez11 <lopezelainy@gmail.com>
2025-04-04 19:28:19 -0500

    wip:define route to create book from bookAPI

--- controllers/books.js
+++ controllers/books.js
@@ -4,6 +4,30 @@ const User = require('../models/user');
 const Book = require('../models/book');
 const router = express.Router();
 
+//TODO: Change Url to reflect universal instead of specific book
+const API_URL='https://www.googleapis.com/books/v1/volumes/wod-BAAAQBAJ'
+
+//POST /book -CREATE New Book Info from API 
+router.post('/', verifyToken, async (req, res) => {
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        const bookInfo = {
+            isbn: data.volumeInfo.industryIdentifiers[0].identifier, //Correct ISBN# ?
+            title: data.volumeInfo.title,
+            author: data.volumeInfo.authors.toString(),
+            description: data.volumeInfo.description,
+            numberOfPages: data.volumeInfo.pageCount,
+            thumbnailUrl: data.volumeInfo.imageLinks.thumbnail,
+        }
+        const book = await Book.create(bookInfo);
+        res.json(book);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: error.message})
+    }
+});
+
 //POST /books - CREATE (Add new Book Stats) Route "Protected"
 router.post('/', verifyToken, async (req, res) => {
     try {
